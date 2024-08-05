019e67b5a52ae00d9b8f6636ff385f622b4dce3e
leonardooliveirati <78557233+leonardooliveirati@users.noreply.github.com>
2024-08-05 13:05:39 -0300

    Aplicado validação de dados com FluentValidation

---
+++ src/Product.Application/DTOs/ProductDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Product.Application.DTOs;
+
+public class ProductDtoValidator : AbstractValidator<ProductDto>
+{
+    public ProductDtoValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .Length(2, 100).WithMessage("Name must be between 2 and 100 characters.");
+
+        RuleFor(p => p.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than 0.");
+    }
+}
